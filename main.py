948dc5e0bd7eca80cdcc04a9bf55f89b7a019c47
Aigerim <omuralieva>
2021-10-12 20:52:27 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+digits = 10, 20, 30  # (число)
+introdusing = "Меня зовут Марина"  # (строка)
+var_boolean_true = True  # Variable boolean(Истина)
+var_boolean_false = False  # Variable boolean(Ложь)
+list_of_digits = [1, 2, 3, 4]  # (список)
+salutation = ("Здравствуйте, мы рады видеть Вас на нашем сайте", "Чем мы можем вам помочь?",)  # (кортеж)
+x = 1.2, 2.3, 4.5  # (числа с плавающей строкой)
+position = {'Виктор': 'директор', 'Марина': 'консультант'}  # (словарь)
+
+
+def print():
+    var_int = 5, 6, 7  # (число)
+    var_str = "Меня зовут Ольга"  # (строка)
+    var_boolean_true = True  # Variable boolean(Истина)
+    var_boolean_false = False  # Variable boolean(Ложь)
+    var_list = [4, 5, 6, 7]  # (список)
+    var_tuple = ("Здравствуйте", "Как ваши дела?",)  # (кортеж)
+    var_float = 1.5, 2.5, 4.6  # (числа с плавающей строкой)
+    var_dict = {'Олег': 'директор', 'Виктория': 'консультант'}  # (словарь)
+
+
+digit = 2
+not_digit = str(digit)
+digit2 = '3'
+not_digit2 = int(digit2)
